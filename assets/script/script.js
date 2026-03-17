ec24e1c30ebc5edc9458c4511fa99ac0ef80a987
DIPAK <dipakparmar2466@gmail.com>
2026-03-17 17:29:50 +0530

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function nextSlide(){
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+
+setInterval(nextSlide,3000);
